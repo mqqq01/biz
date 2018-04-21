6f9bc520c5fd6baad7a6e0aebaed7a3d1a4eedc1
maoqian <maoqian0118@163.com>
2018-04-21 11:16:27 +0800

    修改配置文件

--- src/main/java/com/kuaibao/web/HelloController.java
+++ src/main/java/com/kuaibao/web/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${my.name}")
+    @Value("${com.kuaibao.biz.name}")
     private String name;
 
     @RequestMapping("/hello")
